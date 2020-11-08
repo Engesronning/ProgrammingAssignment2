5514c666d5a0a92bdc8ab1ac56a5867fd3f69af5
Engesrønning <S1909C@corp1.ad1.seb.net>
2020-11-08 07:02:04 +0100

    Commiting my solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## first function creates matrix object that gets and sets value of 
+## a matrix and gets/sets value of the matrix inverse
 
 makeCacheMatrix <- function(x = matrix()) {
+      m <- NULL
+      set <- function(y){
+        x <<- y
+        m <<- NULL
+      }
+      get <- function() x
+      setinverse <- function(solve) m <<- solve
+      getinverse <- function() m
 
+      list( set = set,
+            get = get,
+            setinverse = setinverse,
+            getinverse = getinverse)
+  # print(x)
 }
 
 
-## Write a short comment describing this function
+## Second function checks input matrix it has already been cashed
+## if not previously cashed then invert the input matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## retrieve matrix with getinvesre
+      m <- x$getinverse()
+      
+      
+      ##Checking if input matrix is same as unchanged input matrix
+      if(!is.null(m)){
+          message("Getting chached matrix")
+          return(m)
+      }
+      ## if input not already inverted -> do the job
+      data <- x$get()
+      m <- solve(data,...)
+      x$setinverse(m)
+      m
 }
